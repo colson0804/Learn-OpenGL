bd02b88ef1d8318a7dbe6383a85305b8bd549204
Craig Olson <craig.olson804@gmail.com>
2023-06-08 11:13:51 -0700

    Initial commit

---
+++ Learn-OpenGL/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Learn-OpenGL
+//
+//  Created by Craig Olson on 6/6/23.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
