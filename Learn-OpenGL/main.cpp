187e7a79f4f5ce9c68c670d73ff52a4ceaf3af80
Craig Olson <craig.olson804@gmail.com>
2023-06-12 12:23:56 -0700

    Add colors to triangle verticies

--- Learn-OpenGL/main.cpp
+++ Learn-OpenGL/main.cpp
@@ -16,18 +16,19 @@ void processInput(GLFWwindow *window);
 // shader definitions
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;"
     "void main()\n"
     "{\n"
-    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
+    "gl_Position = vec4(aPos, 1.0);\n"
+    "ourColor = aColor;\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "FragColor = ourColor;\n"
+    "FragColor = vec4(ourColor, 1.0);\n"
     "}\0";
 
 // global constants
@@ -111,9 +112,10 @@ int main(int argc, const char * argv[]) {
     
     // Define vertex data and attributes
     float vertices[] = {
-        0.0f,  0.5f, 0.0f,  // top
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
+        // positions        // colors
+        0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // top
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f  // bottom left
     };
     
     unsigned int VBO, VAO;
@@ -125,8 +127,13 @@ int main(int argc, const char * argv[]) {
     
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     // Clear buffer and vertex array
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -139,12 +146,13 @@ int main(int argc, const char * argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        
         glUseProgram(shaderProgram);
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        
+        // Update the uniform color
+//        float timeValue = glfwGetTime();
+//        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+//        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+//        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
