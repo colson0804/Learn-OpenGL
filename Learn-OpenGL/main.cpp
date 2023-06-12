fdc08b1dea90d5070a9ee22fcccdf2763d1e7607
Craig Olson <craig.olson804@gmail.com>
2023-06-12 11:58:44 -0700

    Back to triangle

--- Learn-OpenGL/main.cpp
+++ Learn-OpenGL/main.cpp
@@ -15,15 +15,18 @@ void processInput(GLFWwindow *window);
 // shader definitions
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;"
     "void main()\n"
     "{\n"
     "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
+    "in vec4 vertexColor;"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "FragColor = vertexColor;\n"
     "}\0";
 
 // global constants
@@ -31,6 +34,7 @@ const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
 
 int main(int argc, const char * argv[]) {
+    // Initialize window and set it to current context
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -53,6 +57,7 @@ int main(int argc, const char * argv[]) {
         std::cout << "Failed to initialize GLAD" << std::endl;
     }
     
+    // Set viewport size to the size of the window
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -105,27 +110,19 @@ int main(int argc, const char * argv[]) {
     
     // Define vertex data and attributes
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
+        0.0f,  0.5f, 0.0f,  // top
         0.5f, -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
     };
     
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -133,7 +130,6 @@ int main(int argc, const char * argv[]) {
     // Clear buffer and vertex array
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     
     while (!glfwWindowShouldClose(window))
     {
@@ -144,9 +140,8 @@ int main(int argc, const char * argv[]) {
         
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         glfwSwapBuffers(window);
